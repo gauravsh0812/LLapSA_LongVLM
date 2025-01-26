ab04377acc8b97236840f46bd17a881ffb4ac5ba
gauravsh0812 <gauravs@arizona.edu>
2025-01-25 23:09:46 -0700

    update

--- scripts/save_features_for_clip.py
+++ scripts/save_features_for_clip.py
@@ -97,11 +97,11 @@ def cross_attention(image_tensor, text_tensor,null_flag):
     output, attention_weights = mha(query=text_tensor, key=image_tensor, value=image_tensor)
     print(attention_weights.shape)
     # attn = torch.flatten(attention_weights, -2, -1)
-    print(attention_weights.sum(dim=1))
+    print(attention_weights.sum(dim=2))
     print(f"Mean: {attention_weights.mean().item()}")
     print(f"Std: {attention_weights.std().item()}")
     exit()
-    
+
     return output
 
 # def cross_attention(image_tensor, text_tensor, null_flag=False):
