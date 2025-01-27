868dde51685e6c3d56875873d855a3059cac9817
gauravsh0812 <gauravs@arizona.edu>
2025-01-27 13:45:22 -0700

    update

--- scripts/save_features.py
+++ scripts/save_features.py
@@ -133,8 +133,9 @@ def process_dino_and_vcgpt_files(x, y):
         # if not os.path.exists(f"{output_path}/{file}"):
         #     try:
         dino_tensors = pickle.load(open(f"{dino_path}/{file}", 'rb'))[:, 1:]
+        print(reduced_tensor.shape)
         reduced_tensor = reduce_similar_frames(dino_tensors) # (20, 256, 1024)
-        print(reduced_tensor)
+        print(reduced_tensor.shape)
         exit()
         spatial_tokens = get_spatio_temporal_features(reduced_tensor)
         # print(spatial_tokens.shape)
