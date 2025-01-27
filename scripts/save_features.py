458e7b57f01349d9f08d4f77f222d83987718f09
gauravsh0812 <gauravs@arizona.edu>
2025-01-27 13:56:37 -0700

    update

--- scripts/save_features.py
+++ scripts/save_features.py
@@ -83,7 +83,7 @@ def reduce_similar_frames(visual_emb_frame):
             dim=-1,
         )
         print("chunk_feature[0]: ", chunk_feature[0].unsqueeze(0).repeat_interleave(len(chunk_feature[1:].shape)))
-
+        print("chunk feature[1:].flatten: ", chunk_feature[1:].flatten(0, 1).shape)
         new_visual_emb_frame = torch.cat(
             [chunk_feature[0],chunk_feature[1:].flatten(0, 1)[sim.flatten(0, 1) < 0.7]],
             dim=0,
