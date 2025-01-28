b7b4a70a61abdd6debf54570faf3b20f9b81fc0f
gauravsh0812 <gauravs@arizona.edu>
2025-01-28 10:25:14 -0700

    update

--- scripts/save_features.py
+++ scripts/save_features.py
@@ -157,8 +157,8 @@ def process_dino_and_vcgpt_files(x, y):
         print("dino: ", dino_tensors.shape)
         reduced_tensor = reduce_similar_frames(dino_tensors) # (15360, 1024)
         print("reduced: ", reduced_tensor.shape)
-        siglip_tensor = siglip(reduced_tensor)
-        print(siglip_tensor)
+        # siglip_tensor = siglip(reduced_tensor)
+        # print(siglip_tensor)
 
         
         # exit()
