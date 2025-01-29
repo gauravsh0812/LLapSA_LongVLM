9d53105b6715df721e2ab679b024769833d4e431
gauravsh0812 <gauravs@arizona.edu>
2025-01-28 21:13:27 -0700

    update

--- scripts/save_features.py
+++ scripts/save_features.py
@@ -7,58 +7,22 @@ import tqdm
 import pickle
 import argparse
 import multiprocessing as mp
-from sklearn.metrics.pairwise import cosine_similarity
 import torch.nn.functional as F
-from transformers import SiglipImageProcessor, SiglipVisionModel, SiglipVisionConfig
-from scripts.base_encoder import BaseVisionTower
 from longvlm.model.merge import merge_tokens
 from scripts.dino_encoder import load_video
+from transformers import CLIPVisionModel, CLIPImageProcessor
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Training")
     parser.add_argument("--xy", required=True)
-    parser.add_argument("--dino_path", required=True, help="Path to read the videos from.")
-    parser.add_argument("--output_path", required=True)
+    parser.add_argument("--dino_path", required=True, help="Path to read the dino tensors from.")
+    parser.add_argument("--video_path", required=True, help="Path to read the videos from.")
+    parser.add_argument("--local_feature_path", required=True, help="path to save local features")
+    parser.add_argument("--global_feature_path", required=True, help="path to save global features")
     args = parser.parse_args()
     return args
 
-# class SiglipVisionTower(BaseVisionTower):
-#     def __init__(self, vision_tower_name="google/siglip-so400m-patch14-384", delay_load=False):
-#         super(SiglipVisionTower, self).__init__(vision_tower_name, delay_load)
-        
-#         model_path = vision_tower_name
-#         base_model_name, res, interp = model_path, 384, 576
-#         self.vision_tower_name = base_model_name
-#         self._image_size = res if res is not None else 512
-#         self._interp_size = interp
-#         if not self.delay_load:
-#             self.load_model()
-#         elif self.unfreeze_mm_vision_tower:
-#             self.load_model()
-#         else:
-#             self._hidden_size = 1024
 
-#     def load_model(self, device_map=None):
-#         self.vision_tower = SiglipVisionModel.from_pretrained(self.vision_tower_name)
-#         self.vision_tower.output_tokens = True
-
-#         self._hidden_size = self.vision_tower.config.hidden_size
-#         self._image_size = self.vision_tower.config.image_size
-#         self._patch_size = self.vision_tower.config.patch_size
-#         self.image_processor = SiglipImageProcessor.from_pretrained(
-#             self.vision_tower_name
-#         )
-
-#         self.vision_tower.requires_grad_(self.unfreeze_mm_vision_tower)
-#         self.is_loaded = True
-
-#     def forward(self, images,):
-#         with torch.set_grad_enabled(self.unfreeze_mm_vision_tower):
-#             image_features = self.vision_tower.forward(
-#                 images.to(device=self.device, dtype=self.dtype),
-#                 output_hidden_states=True, interpolate_pos_encoding=True,
-#             ).hidden_states[-1]
-#             return image_features
 
 def reduce_similar_frames(visual_emb_frame):
     
@@ -150,12 +114,17 @@ def process_dino_and_vcgpt_files(x, y):
     # Fetch all files from args.dino_path and args.vcgpt_features_path
     args = parse_args()
     dino_path = args.dino_path
+    video_path = args.video_path
     dino_files = os.listdir(dino_path)[x:y]
     output_path = args.output_path
 
-    print("dino path: ", dino_path)
-
-    # siglip = SiglipVisionTower()
+    # Initialize the CLIP model
+    image_processor = CLIPImageProcessor.from_pretrained('openai/clip-vit-large-patch14', 
+                                                         torch_dtype=torch.float16)
+    vision_tower = CLIPVisionModel.from_pretrained('openai/clip-vit-large-patch14', 
+                                                   torch_dtype=torch.float16,
+                                                   low_cpu_mem_usage=True).cuda()
+    vision_tower.eval()
 
     # Load pickled tensors
     reduced_tensors = []
@@ -163,27 +132,31 @@ def process_dino_and_vcgpt_files(x, y):
         # if not os.path.exists(f"{output_path}/{file}"):
         #     try:
         dino_tensors = pickle.load(open(f"{dino_path}/{file}", 'rb'))[:,1:,:]
-        # print("dino: ", dino_tensors.shape)
         reduced_tensor = reduce_similar_frames(dino_tensors) # (60, 256, 1024)
-        # print("reduced: ", reduced_tensor.shape)
-        # reduced_tensor = reduced_tensor.unsqueeze(1).repeat(1, 3, 1, 1)
-        # print("reduced: ", reduced_tensor.shape)
-    
-        # siglip_input = torch.cat(reduced_tensors, dim=0)
-        # print(siglip_input.shape)
-        # siglip_tensor = siglip(siglip_input)
-        # print(siglip_tensor.shape)
 
+        # spatial features from clip forr frames 
+        # one for 12 frames
+        arr = [19,39,59,79,99]
+        frames = load_video(open(f"{video_path}/{file.replace('.pkl','.mp4')}"))        
+        frames = torch.stack([frames[i] for i in arr], dim=0)
+        print(frames.shape)
+        exit()
+        video_tensor = image_processor.preprocess(frames, return_tensors='pt')['pixel_values']
+        video_tensor = video_tensor.half()
+        image_forward_outs = vision_tower(video_batch, output_hidden_states=True)
+        select_hidden_state_layer = -2
+        select_hidden_state = image_forward_outs.hidden_states[select_hidden_state_layer]
+        batch_features = select_hidden_state[:, 1:]
+
+        window = int(reduced_tensor.shape[0] // len(arr))
+
+        
         # local features
         local_feat = merge_tokens(
             reduced_tensor, 
             r_merge_list=[2880, 1440, 720, 360, 180, 90, 40]
         ).detach().cpu().numpy().astype("float16")  # [1280, 640, 320, 160, 80, 40, 10]
         print(local_feat.shape)
-
-        # spatial features from clip forr frames 
-        arr = [19,39,59,79,99]
-
         
 if __name__ == "__main__":
     args = parse_args()
