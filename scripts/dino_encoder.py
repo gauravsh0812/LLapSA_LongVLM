b0b52455d0899b919d27bef1d0b80c60c23330d6
gauravsh0812 <gauravs@arizona.edu>
2025-01-28 23:25:08 -0700

    update

--- scripts/dino_encoder.py
+++ scripts/dino_encoder.py
@@ -141,20 +141,20 @@ def main():
         video_id = video_name.split('.')[0]
         if os.path.exists(f"{vcgpt_features}/{video_id}.pkl"):
             continue
-        try:
-            frames = load_video(video_path)
-            preprocessed_frames = dino.preprocess_frames(frames)
-            features = dino.extract_features(preprocessed_frames, layer_index=-2)
-            video_clip_features[video_id] = features.half()
-            counter += 1       
+        # try:
+        frames = load_video(video_path)
+        preprocessed_frames = dino.preprocess_frames(frames)
+        features = dino.extract_features(preprocessed_frames, layer_index=-2)
+        video_clip_features[video_id] = features.half()
+        counter += 1       
 
-            # getting global features
-            if not os.path.exists(f"{global_feature_path}/{video_id}.pkl"):
-                memory_features[video_id] = torch.cat([mem[:, :1] for mem in features.hidden_states], 
-                                                      dim=1).mean(0).squeeze(0).detach().cpu().numpy().astype("float16")
-            
-        except Exception as e:
-            print(f"Can't process {video_path} due to {e}")
+        # getting global features
+        if not os.path.exists(f"{global_feature_path}/{video_id}.pkl"):
+            memory_features[video_id] = torch.cat([mem[:, :1] for mem in features.hidden_states], 
+                                                    dim=1).mean(0).squeeze(0).detach().cpu().numpy().astype("float16")
+        
+        # except Exception as e:
+        #     print(f"Can't process {video_path} due to {e}")
     
         if counter % 50==0:
             for key in video_clip_features.keys():
