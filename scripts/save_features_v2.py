35de969ff67e769ba8677b9f025acf63ff45f634
gauravsh0812 <gauravs@arizona.edu>
2025-01-30 14:28:13 -0700

    update

--- scripts/save_features_v2.py
+++ scripts/save_features_v2.py
@@ -94,7 +94,7 @@ def main():
     video_features = {}
     memory_features = {}
     counter = 0
-    for video_name in tqdm(all_videos[3000:4000]):
+    for video_name in tqdm(all_videos[4000:5500]):
         video_path = f"{video_dir_path}/{video_name}"
         video_id = video_name.split('.')[0]
 
